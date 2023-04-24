734a25119cacae0e11bacd99d0b3dc81dbe57cc4
developerankit97 <ankitsh.developer@gmail.com>
2023-04-25 00:54:10 +0530

    updated validation in signup form

--- public/signup.js
+++ public/signup.js
@@ -2,32 +2,44 @@ const signup = document.querySelector('.signup');
 const name = document.querySelector('#name');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
 
 signup.addEventListener('click', userSignup);
 email.addEventListener('keydown', validateEmail);
 
 async function userSignup(e) {
     e.preventDefault();
-    try {
-        if (name.value && email.value && password.value) {
-            const response = await axios.post('http://localhost:3000/user/signup', { "name": name.value, "email": email.value, "password": password.value });
-            console.log(response);
-        } else {
-            alert('Please fill in all fields');
+    if (matchPassword(password.value, confirmPassword.value)) {
+        try {
+            if (name.value && email.value && password.value) {
+                const response = await axios.post('http://localhost:3000/user/signup', { "name": name.value, "email": email.value, "password": password.value });
+                console.log(response);
+            } else {
+                alert('Please fill in all fields');
+            }
+        } catch (error) {
+            alert('User Already Exists');
         }
-    } catch (error) {
-        console.log(error);
+    } else {
+        alert('Passwords do not match');
     }
+
+}
+
+function matchPassword(password, confirmPassword) {
+    if (password === confirmPassword) {
+        return true;
+    }
+    return false;
 }
 
 function validateEmail(event) {
     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    console.log(email.value, event);
     if (email.value.match(validRegex)) {
-        email.style.backgroundColor = 'green';
+        email.style.backgroundColor = 'white';
         return true;
     } else {
-        email.style.backgroundColor = 'red';
+        email.style.backgroundColor = '#FA7E7C';
         return false;
     }
 }
